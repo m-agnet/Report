61d94e53195c4b3d5bb52ebb04121767ea6d5f44
山本凜 <karon0617@gmail.com>
2023-11-22 07:05:24 +0900

    updated

--- src/plot_LJpotential.jl
+++ src/plot_LJpotential.jl
@@ -1,4 +1,4 @@
-# LJポテンシャルの描画.
+# 汎用LJポテンシャル描画セル.
 # パッケージ.
 using Plots
 
@@ -19,14 +19,21 @@ sigma = 1.0
 rc = 3.0 * sigma
 
 # Rd, Rt, Raの配列.
-Rd_values = range(0.0,length=1)
-Rt_values = range(0.5,length=1)
-Ra_values = range(3.0-2^(1/6),length=1)
+Rd_values = range(0.0, length=1)
+Rt_values = range(0.0,0.5, length=5)
+Ra_values = range(1.877, length=1)
 
-# プロット設定.
-plt=plot(xlims=(0.75, 3.0), ylims=(-1.5, 3.5), xlabel="r/σ", ylabel="ϕ/ε", title="LJ-Potential vs. r", show=true)
+# プロット概形.
+plot(xlabel="r/σ", ylabel="ϕ/ε")
+xlims!(0.2,2.5)
+ylims!(-1.5,3.0)
+title!("LJ-Potential vs. r")
+xlabel!("r/σ")
+ylabel!("ϕ/ε")
+
 # 粒子-粒子LJポテンシャルのプロット.
 plot!(r -> phi_tilde(r, epsilon, sigma, rc), label="Potential_pair; ε=$(round(epsilon,digits=1)), σ=$(round(sigma,digits=1)), rc=$(round(3.0,digits=2))σ", linestyle=:dash)
+
 # プロットの追加.
 for Rd in Rd_values, 
     Rt in Rt_values, 
@@ -34,12 +41,16 @@ for Rd in Rd_values,
     # 壁-粒子LJポテンシャルのパラメータ.
     epsilon_wall = (1.0 - Rd) * epsilon
     sigma_wall = (0.5 + Rt) * sigma
-    rc_wall = ((2^(1/6)) + Ra) * sigma_wall
+    rc_wall = ((2 ^ (1 / 6)) + Ra) * sigma_wall
+    # 打つ点を調整.
+    x_values = range(Rt+0.3,3.0,length=10000) 
+    y_values = phi_tilde.(x_values, epsilon_wall, sigma_wall, rc_wall) 
     # 壁-粒子LJポテンシャルのプロット.
-    plot!(r -> phi_tilde(r, epsilon_wall, sigma_wall, rc_wall), label="Potential_wall; εw=$(round(epsilon_wall,digits=1))ε, σw=$(round(sigma_wall,digits=1))σ, rcw=$(round((2^(1/6)) + Ra,digits=2))σw", linestyle=:dash)
+    plot!(x_values, y_values, label="Potential_wall; εw=$(round(epsilon_wall,digits=1))ε, σw=$(round(sigma_wall,digits=1))σ, rcw=$(round((2^(1/6)) + Ra,digits=2))σw=$(round(((2^(1/6)) + Ra)*sigma_wall,digits=2))σ", linestyle=:dash)
 end
-display(plt)
-# savefig("LJpotential.png")
+
+display(plot!())
+# savefig("")
 
 ccall(:jl_tty_set_mode, Int32, (Ptr{Cvoid}, Int32), stdin.handle, true)
 read(stdin, 1)
